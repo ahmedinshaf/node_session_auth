f9ffff530d7b27a6e488189634c26a86ad5d238f
Inshaf Ahamed <amdinshaf@gmail.com>
2021-03-12 12:30:32 +0530

    db name changed

--- source/config/db.ts
+++ source/config/db.ts
@@ -1,5 +1,5 @@
 import { ConnectionOptions } from 'mongoose';
-export const MONGO_URI = `mongodb+srv://inshaf:127149@cluster0.t0rre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+export const MONGO_URI = `mongodb+srv://inshaf:127149@cluster0.t0rre.mongodb.net/Stablo?retryWrites=true&w=majority`;
 
 export const MONGO_OPTIONS: ConnectionOptions = {
     useNewUrlParser: true,
